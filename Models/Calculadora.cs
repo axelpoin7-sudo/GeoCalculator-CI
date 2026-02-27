48e2774175d3c001d3babc0cc92ccd0c52670036
LABORATORIO A <LABORATORIO A@LAB.EDU.BO>
2026-02-27 12:00:06 -0400

    Circulo

---
+++ Models/Calculadora.cs
@@ -0,0 +1,10 @@
+using System;
+namespace GeoApp.Models
+{
+    public static class Calculadora
+    {
+        public static double AreaRectangulo(double baseR, doublealtura) => baseR * altura;
+        public static double AreaCirculo(double radio) => Math.PI *Math.Pow(radio, 2);
+        public static double AreaTriangulo(double baseT, doublealtura) => (baseT * altura) / 2;
+    }
+}
