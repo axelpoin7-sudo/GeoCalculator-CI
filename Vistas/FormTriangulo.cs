3b82a77e359a3f50bd09371e70c81856e466662b
UserNameXD679 <soloscuro466@gmail.com>
2026-02-27 13:12:38 -0400

    Add files via upload

---
+++ Vistas/FormTriangulo.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using GeoApp.Models;
+namespace GeoApp
+{
+    public class FormRectangulo : Form
+    {
+        private TextBox txtBase = new TextBox { Location = newPoint(100, 20) };
+        private TextBox txtAltura = new TextBox { Location = newPoint(100, 50) };
+        private Button btnCalcular = new Button { Text ="Calcular", Location = new Point(100, 80) };
+        public FormRectangulo()
+        {
+            this.Controls.Add(new Label { Text = "Base:", Location= new Point(20, 20) });
+            this.Controls.Add(txtBase);
+            this.Controls.Add(new Label { Text = "Altura:",Location = new Point(20, 50) });
+            this.Controls.Add(txtAltura);
+            this.Controls.Add(btnCalcular);
+            btnCalcular.Click += (s, e) => {double res =Calculadora.AreaTriangulo(double.Parse(txtBase.Text),double.Parse(txtAltura.Text));
+            MessageBox.Show("El área es: " + res);
+            };
+        }
+    }
+}
